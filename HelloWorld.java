bb4e1c9f4e54a049d5b77d2c04a14003d33ac6e1
ommakhana <ommakhana28@gmail.com>
2025-11-16 10:24:04 +0000

    Fix formatting and output in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-echo 'public class HelloWorld{
- public static void main (String[] args){
-  System.out.println("Hello, World!");
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
-}'>HelloWorld.java
